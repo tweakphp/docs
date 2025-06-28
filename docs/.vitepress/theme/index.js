9bdf74670c4bb2badb7ec5be14e352e87f1591bb
David Galet <davidgalet@gmail.com>
2025-06-28 23:48:48 +0200

    refactor: remove unused enhanceApp method and clean up imports

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,4 @@
 // https://vitepress.dev/guide/custom-theme
-import {h} from 'vue'
 import Theme from 'vitepress/theme'
 import Contributors from './components/Contributors.vue'
 import './style.css'
@@ -8,8 +7,4 @@ import MyLayout from "./MyLayout.vue";
 export default {
     extends: Theme,
     Layout: MyLayout,
-    enhanceApp({app, router, siteData}) {
-        // app.component('Component', Component)
-        app.component('Contributors', Contributors)
-    },
 }
